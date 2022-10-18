7f458d6fc302ae081d1a29086077f8cf6868d1c9
Alabi Temitope <t.banji@rocketmail.com>
2022-10-18 15:42:45 +0100

    updated

---
+++ src/components/notfound/notFound.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const NotFound = () => (
+  <div>
+    <h1>Not Found page</h1>
+    <p>The page you tried to access doesn't exist.</p>
+    <p>This is a generic route.</p>
+  </div>
+);
+
+export default NotFound;
